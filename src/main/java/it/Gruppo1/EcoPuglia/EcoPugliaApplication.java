448d121c155c97aa0136f37444a1bb1af5e5a92b
Andrea Cotugno <andreacotu11@gmail.com>
2024-03-08 19:01:18 +0100

    Primo commit
    Default Springboot inizialization

---
+++ src/main/java/it/Gruppo1/EcoPuglia/EcoPugliaApplication.java
@@ -0,0 +1,13 @@
+package it.Gruppo1.EcoPuglia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPugliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoPugliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/Gruppo1/EcoPuglia/EcoPugliaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.Gruppo1.EcoPuglia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoPugliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
