4066db52991ff6ed6bdbcc597f731c90d6d44c13
IlCotugamer <andreacotu11@gmail.com>
2024-03-11 08:36:47 +0100

    Aggiungo TODO
    Todo aggiunto per migliorare l'organizzazione del progetto

--- src/main/java/it/Gruppo1/EcoPuglia/controller/RestApiController.java
+++ src/main/java/it/Gruppo1/EcoPuglia/controller/RestApiController.java
@@ -1,5 +1,11 @@
 package it.Gruppo1.EcoPuglia.controller;
 
+/*  Rest Api controller
+TODO:
+    - Lettura db
+    - Richieste al db
+    - Query specifiche
+*/
 public class RestApiController {
 
 }
