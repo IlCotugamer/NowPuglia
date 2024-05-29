bbc543f3ce191777f81229e77653d22ee1c2b646
Luca Petruzzelli <lucapetruzzelli34@gmail.com>
2024-05-29 18:06:38 +0200

    Commit x presentazione progetto con mappa, pin e cerchi funzionanti

--- NowPuglia-FE/js/map.js
+++ NowPuglia-FE/js/map.js
@@ -9,5 +9,86 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 var marker = L.marker([40.828848, 16.561015], { alt: "Altamura" })
   .addTo(map)
   .bindPopup(
-    "Altamura<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti:<br> Via Santeramo:<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+    "Altamura (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> Via Santeramo:<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
   );
+
+  var marker = L.marker([41.113542, 16.888064], { alt: "Bari - Caldarola" })
+  .addTo(map)
+  .bindPopup(
+    "Bari - Caldarola (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([41.076666, 16.866944], { alt: "Bari - Carbonara" })
+  .addTo(map)
+  .bindPopup(
+    "Bari - Carbonara (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([41.122269, 16.872552], { alt: "Bari - Cavour" })
+  .addTo(map)
+  .bindPopup(
+    "Bari - Cavour (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([40.953154, 16.920731], { alt: "Casamassima" })
+  .addTo(map)
+  .bindPopup(
+    "Casamassima (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([41.107777, 16.766111], { alt: "Modugno" })
+  .addTo(map)
+  .bindPopup(
+    "Modugno (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([41.201101, 16.605253], { alt: "Molfetta" })
+  .addTo(map)
+  .bindPopup(
+    "Molfetta (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([41.318333, 16.293055], { alt: "Barletta" })
+  .addTo(map)
+  .bindPopup(
+    "Barletta (BAT)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([40.630917, 17.921778], { alt: "Brindisi" })
+  .addTo(map)
+  .bindPopup(
+    "Brindisi (BA)<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([40.459696, 18.116386], { alt: "Lecce" })
+  .addTo(map)
+  .bindPopup(
+    "Lecce<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([40.460553, 17.263602], { alt: "Taranto" })
+  .addTo(map)
+  .bindPopup(
+    "Taranto<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var marker = L.marker([41.455555, 15.548611], { alt: "Foggia" })
+  .addTo(map)
+  .bindPopup(
+    "Foggia<br><br> Questo comune HA richiesto la connessione alle fonti di energia rinnovabile (eolico on-shore)<br><br> Misurazioni inquinanti (dati di ieri):<br> NO2: 200 µg/m³<br> O3: 180 µg/m³<br> PM10: 50 µg/m³<br> PM2.5: 25 µg/m³<br>"
+  );
+
+  var circle = L.circleMarker([41.455555, 15.548611], {radius: 20}).addTo(map);
+  circle.setStyle({color: 'green'});
+
+  var circle = L.circleMarker([40.828848, 16.561015], {radius: 20}).addTo(map);
+  circle.setStyle({color: 'green'});
+
+  var circle = L.circleMarker([41.122269, 16.872552], {radius: 20}).addTo(map);
+  circle.setStyle({color: 'green'});
+
+  var circle = L.circleMarker([40.630917, 17.921778], {radius: 20}).addTo(map);
+  circle.setStyle({color: 'green'});
+
+  var circle = L.circleMarker([41.318333, 16.293055], {radius: 20}).addTo(map);
+  circle.setStyle({color: 'green'});
